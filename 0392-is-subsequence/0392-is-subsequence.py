0aed796f41675506b7aed71a0fdeeffe14a07a56
Fiza Ashraf <68822806+fizaashraf37@users.noreply.github.com>
2025-12-07 21:29:11 -0500

    Time: 0 ms (100%), Space: 18 MB (24.25%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -9,8 +9,8 @@ class Solution:
         for j in range(len(t)):
             if s[i] == t[j]:
                 i += 1
-                if i == len(s):
-                    break
+            if i == len(s):
+                break
         
         return i == len(s)
 
