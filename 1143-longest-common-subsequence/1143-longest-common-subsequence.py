a0ad60fd6dec63152ee6df9a8c365d30a4c62fac
Fiza Ashraf <68822806+fizaashraf37@users.noreply.github.com>
2025-12-07 22:39:50 -0500

    Time: 406 ms (87.59%), Space: 16.8 MB (100%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,24 +1,21 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-
-        def lcs(i: int, j: int, memo: dict = {}) -> int:
-
-            if i == len(text1) or j == len(text2):
-                return 0
-            
-            if (i,j) in memo:
-                return memo[(i,j)]
-            
-            lcs_length = 0
-
-            if text1[i] == text2[j]:
-                lcs_length =  1 + lcs(i+1, j+1, memo)
-            else:
-                lcs_length = max(lcs(i+1, j, memo), lcs(i, j+1, memo))
-            
-            memo[(i,j)] = lcs_length
-            
-            return lcs_length
         
-        return lcs(0,0, {})
+        prev_row = [0]*(len(text2)+1)
+        cur_row = [0]*(len(text2)+1)
         
+        for i in range(1, len(text1)+1):
+            for j in range(1, len(text2)+1):
+                if text1[i-1] == text2[j-1]:
+                    cur_row[j] = prev_row[j-1]+1
+                else:
+                    cur_row[j] = max(prev_row[j], cur_row[j-1])
+            
+            prev_row, cur_row = cur_row, prev_row
+        
+        return prev_row[-1]
+        
+        #   a b c d e a c
+        # a 1 1 1 1 1 1 1
+        # c 1 1 2 2 2 2 2
+        # e 1 1 2 2 3
