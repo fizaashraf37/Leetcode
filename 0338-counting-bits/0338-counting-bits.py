7063cb75b1459d6e5c828eed6e1de1dfd4fa50fc
Fiza Ashraf <68822806+fizaashraf37@users.noreply.github.com>
2025-12-09 05:19:42 -0500

    Time: 3 ms (94.47%), Space: 18.6 MB (21.08%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -5,12 +5,10 @@ class Solution:
             return [0]
 
         ans = [0,1]
-        bits_range = 2
 
         while len(ans) < n+1:
-            for i in range(min(bits_range, n-len(ans)+1)):
+            for i in range(min(len(ans), n-len(ans)+1)):
                 ans.append(ans[i]+1)
-            bits_range = bits_range << 1
             
         return ans
 
