b53fb0e612d97f2dddcd54533fa9edc7e3e8ca04
Fiza Ashraf <68822806+fizaashraf37@users.noreply.github.com>
2025-12-11 06:40:59 -0500

    Time: 419 ms (87.4%), Space: 56 MB (86.55%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.py
+++ 3531-count-covered-buildings/3531-count-covered-buildings.py
@@ -1,33 +1,31 @@
 class Solution:
     def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
 
-        buildings.sort(key=lambda x: (x[0],x[1]))
-        covered_buildings = set()
+        left_col = [n+1]*(n+1)
+        right_col = [0]*(n+1)
+        top_row = [0]*(n+1)
+        bottom_row = [n+1]*(n+1)
 
-        for i in range(1, len(buildings)-1):
-            if buildings[i-1][0] == buildings[i][0] and buildings[i+1][0] == buildings[i][0]:
-                covered_buildings.add(str(buildings[i][0]) + ":" + str(buildings[i][1]))
+        for x, y in buildings:
+            left_col[y] = min(left_col[y], x)
+            right_col[y] = max(right_col[y], x)
+            top_row[x] = max(top_row[x], y)
+            bottom_row[x] = min(bottom_row[x], y)
         
-        buildings.sort(key=lambda x: (x[1],x[0]))
-        # print(covered_buildings)
-
-        for i in range(1, len(buildings)-1):
-            if buildings[i-1][1] == buildings[i][1] and buildings[i+1][1] == buildings[i][1]:
-                continue
-            building = str(buildings[i][0]) + ":" + str(buildings[i][1])
-            if building in covered_buildings:
-                covered_buildings.remove(building)
+        count = 0
+        for x, y in buildings:
+            if left_col[y] < x < right_col[y] and bottom_row[x] < y < top_row[x]:
+                count += 1
         
-        first_building = str(buildings[0][0]) + ":" + str(buildings[0][1])
-        if first_building in covered_buildings:
-            covered_buildings.remove(first_building)
-        last_building = str(buildings[-1][0]) + ":" + str(buildings[-1][1])
-        if last_building in covered_buildings:
-            covered_buildings.remove(last_building)
+        return count
+
+
+
+
+
+
+
+
         
-        return len(covered_buildings)
-
-
-        # 1,2  2,1  2,2  2,3  3,2
 
         
