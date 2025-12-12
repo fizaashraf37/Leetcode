dfba1ae98bd60cedafcd128f4643008693cba553
Fiza Ashraf <68822806+fizaashraf37@users.noreply.github.com>
2025-12-12 05:11:39 -0500

    Time: 0 ms (100%), Space: 17.7 MB (91.63%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.py
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.py
@@ -7,11 +7,8 @@
 class Solution:
     def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
 
-        if not root1 and not root2:
-            return True
-        
-        if (root1 and not root2) or (root2 and not root1):
-            return False
+        if not root1 or not root2:
+            return root1 == root2
         
         if root1.val == root2.val:
             return (self.flipEquiv(root1.left, root2.right) and self.flipEquiv(root1.right, root2.left)) or (self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right))
