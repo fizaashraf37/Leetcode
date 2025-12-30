d96ac3c0e50f0111cb852ebda814481458c9c128
Fiza Ashraf <68822806+fizaashraf37@users.noreply.github.com>
2025-12-29 22:19:01 -0500

    Time: 19 ms (15.36%), Space: 17.6 MB (93.4%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
+        if len(s) != len(t):
+            return False
+
         hash_map = Counter(s)
 
         for char in t:
-            if char not in hash_map:
+            if char not in hash_map or hash_map[char] == 0:
                 return False
             hash_map[char] -= 1
-            if hash_map[char] == 0:
-                hash_map.pop(char)
         
-        return len(hash_map) == 0
+        return True
         
