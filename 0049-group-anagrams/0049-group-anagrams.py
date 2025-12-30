49a3ec92f32db4ff423a5318c57534d47951c5d7
Fiza Ashraf <68822806+fizaashraf37@users.noreply.github.com>
2025-12-29 23:01:09 -0500

    Time: 49 ms (5.1%), Space: 22.2 MB (30.52%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,24 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
+        def anagram_count(s: str) -> tuple:
+            char_count = {ch: 0 for ch in string.ascii_lowercase}
+            for char in s:
+                char_count[char] += 1
+            return tuple(char_count[ch] for ch in string.ascii_lowercase)
+
         hash_map = {}
         ans = []
 
-        for str in strs:
-            sorted_str = "".join(sorted(str))
-            if sorted_str in hash_map:
-                group_id = hash_map[sorted_str]
-                ans[group_id].append(str)
+        for s in strs:
+            anagram_counts = anagram_count(s)
+            if anagram_counts in hash_map:
+                group_id = hash_map[anagram_counts]
+                ans[group_id].append(s)
             else:
                 group_id = len(hash_map)
-                hash_map[sorted_str] = group_id
-                ans.append([str])
+                hash_map[anagram_counts] = group_id
+                ans.append([s])
         
         return ans
             
