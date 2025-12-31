c54f29467bbf4adba06ae5ab16544dbe7dc1788c
Fiza Ashraf <68822806+fizaashraf37@users.noreply.github.com>
2025-12-31 05:05:44 -0500

    Time: 15 ms (51.48%), Space: 19.2 MB (60.43%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -6,22 +6,19 @@ class Solution:
 
         while left < right:
             cur_min_height = min(height[left], height[right])
+            trapped_water = 0
             if height[left] < height[right]:
-                trapped_water = 0
                 left += 1
                 while left < right and height[left] < cur_min_height:
                     trapped_water += cur_min_height - height[left]
                     left += 1
-                if left <= right:
-                    total_trapped_water += trapped_water
             else:
-                trapped_water = 0
                 right -= 1
                 while left < right and height[right] < cur_min_height:
                     trapped_water += cur_min_height - height[right]
                     right -= 1
-                if left <= right:
-                    total_trapped_water += trapped_water
+            if left <= right:
+                total_trapped_water += trapped_water
         
         return total_trapped_water
                 
